fef8ef0dedeab69a91c4ac8ceadad104a76dffb0
Fauzdar Dipace <dipace.fauzdar@gmail.com>
2020-08-13 09:37:23 +0545

    - Added theme files for kilroyblockchain

---
+++ app/design/frontend/Kilroy/kilroy/Magento_Theme/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    paths: {
+            'simplebanner': "Magento_Theme/js/simpleBanner"
+        },
+    shim: {
+        'simplebanner': {
+            deps: ['jquery']
+        }
+    }
+};
